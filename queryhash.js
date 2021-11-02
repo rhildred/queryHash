2a69a24695c6f0f41ee0a7466c64c50fe56b66c6
Rich Hildred <rhildred@gmail.com>
2021-11-02 18:51:23 -0400

    add code

---
+++ queryhash.js
@@ -0,0 +1,14 @@
+const getQueryHash = ()=>{
+    let oQueries = {};
+    let aQueries = document.location.search.substr(1).split('&');
+    for(let n = 0; n < aQueries.length; n++){
+      let aNvp = aQueries[n].split('=');
+      try{
+        oQueries[aNvp[0].toString()] = aNvp[1].toString();
+      }catch(e){
+          console.log(e.toString())
+      }
+    }
+    console.log(oQueries);
+    return oQueries;
+}
